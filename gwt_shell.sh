ef94882e97accd84e53f834cc327f4104870f55f
Casper S <bad33ndj3@users.noreply.github.com>
2025-07-07 15:35:20 +0200

    Add argument check to gwtcd

--- gwt_shell.sh
+++ gwt_shell.sh
@@ -4,6 +4,10 @@
 # Changes directory to the worktree for <branch>
 
 function gwtcd() {
+    if [[ $# -eq 0 ]]; then
+        echo "Usage: gwtcd <branch>" >&2
+        return 1
+    fi
     cd "$(gwt "$1")" || return
 }
 
